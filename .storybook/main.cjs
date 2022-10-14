7b6f47580c29c142982844b9c5ae41fb4930919c
joaoalvesxp <joaoalvesxp@alu.ufc.br>
2022-10-14 01:06:17 -0300

    After Deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,3 @@
-const { config } = require("@storybook/addon-actions");
-
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -9,18 +7,22 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true
   },
+  "staticDirs": [
+    "../public"
+  ],
   viteFinal: (config, { configType }) => {
-    if (configType == 'PRODUCTION') {
-      config.base = 'ignite-lab-03-design-system'
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
     }
 
     return config
